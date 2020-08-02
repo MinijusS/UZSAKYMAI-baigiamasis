6cdf6abd1d5d4810d42d49b2b9be2d0b03b3c89b
MinijusS <savickasminijus1@gmail.com>
2020-08-02 14:43:01 +0300

    fixed images

--- public_html/index.php
+++ public_html/index.php
@@ -37,6 +37,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+$app->bind('path.public', function() {
+    return realpath(__DIR__); });
+
+
 /*
 |--------------------------------------------------------------------------
 | Run The Application
